f1f21c5b89d7781101f6a9097ac3796246fb1cb7
lior gishry <liorgish@gmail.com>
2019-06-22 23:49:26 +0300

    solution

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,42 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  static AudioCache player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              gen_btn(Colors.red, 1),
+              gen_btn(Colors.orange, 2),
+              gen_btn(Colors.yellow, 3),
+              gen_btn(Colors.green, 4),
+              gen_btn(Colors.teal, 5),
+              gen_btn(Colors.blue, 6),
+              gen_btn(Colors.purple, 7),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Expanded gen_btn(Color color, int note) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        child: null,
+        onPressed: () {
+          player.play('note$note.wav');
+        },
       ),
     );
   }
